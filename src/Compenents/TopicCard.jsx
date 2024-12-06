6cd6682b2b5890a69ced6379ed8b94ab0bba5f02
Benedict Robinson <benedict.r1713@gmail.com>
2024-12-06 13:09:49 +0000

    topic boxes

--- src/Compenents/TopicCard.jsx
+++ src/Compenents/TopicCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function TopicCard({topic}) {
   return (
     <Link to={`/${topic.slug}/articles`}>
-    <li>
+    <li id="topic-card">
         <h2>{topic.slug}</h2>
         <p>{topic.description}</p>
     </li>
--- src/Compenents/TopicsList.jsx
+++ src/Compenents/TopicsList.jsx
@@ -3,7 +3,7 @@ import TopicCard from "./TopicCard"
 export default function TopicsList({topics}) {
 
   return (
-    <ul>
+    <ul className="topics-list">
         {topics.map((topic, index) => {
             return <TopicCard key={index} topic={topic}/>
         })}
