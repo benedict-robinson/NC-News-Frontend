0a866b4628bb90ddc3d9051566d8bfc78cb109f1
Benedict Robinson <benedict.r1713@gmail.com>
2024-12-13 11:17:17 +0000

    Styled New Topic Page

--- src/Compenents/NewTopic.jsx
+++ src/Compenents/NewTopic.jsx
@@ -2,15 +2,20 @@ import { useContext, useState } from "react"
 import { postNewTopic } from "../api";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "./UserContext";
+import "../CSS/new-topic-button.css"
 
 export default function NewTopic() {
     const [topicObj, setTopicObj] = useState({})
+    const [isDisabled, setIsDisabled] = useState(true)
     const { user } = useContext(UserContext)
     const navigate = useNavigate()
 
     function handleChange({target}) {
         const key = target.id
-        setTopicObj({ ...topicObj, [key]: target.value });
+        setTopicObj({ ...topicObj, [key]: target.value })
+        if (topicObj.slug && isDisabled) {
+            setIsDisabled(false)
+        }
     }
 
     const handleSubmit = (event) => {
@@ -28,17 +33,21 @@ export default function NewTopic() {
     <div className="new-topic-page">
         <h2>What do you want to talk about?</h2>
         <form>
+            <div className="topic-input">
             <label>
                 Topic Title: &nbsp;
             </label>
             <input type="text" id="slug" onChange={handleChange}></input>
+            </div>
             <br></br>
+            <div className="topic-input">
             <label>
                 Description: &nbsp;
             </label>
             <input type="text" id="description" onChange={handleChange}></input>
+            </div>
             <br></br>
-            <button onClick={handleSubmit}>Create</button>
+            <button id="new-topic-button" onClick={handleSubmit} disabled={isDisabled}>Create</button>
         </form>
     </div>
   )
