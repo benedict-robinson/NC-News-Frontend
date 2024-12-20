7582ff85aaaa8a5057be7c122397a9fb8dee9fb4
Benedict Robinson <benedict.r1713@gmail.com>
2024-12-20 18:33:17 +0000

    Styled Articles Posted Page

--- src/Compenents/ArticlePostedAlert.jsx
+++ src/Compenents/ArticlePostedAlert.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom"
+import "../CSS/article-posted-buttons.css"
 
 export default function ArticlePostedAlert({setShowAlert, id}) {
   return (
-    <div>
+    <div className="article-posted-page">
         <h2>Article Posted</h2>
+        <div>
         <Link to={`/articles/${id}`}>
         <button>Go To Article</button>
         </Link>
         <button onClick={() => {setShowAlert(false)}}>Write Another</button>
+        </div>
     </div>
   )
 }
