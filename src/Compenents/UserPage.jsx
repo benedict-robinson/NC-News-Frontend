7894df8ab9bc262c75c4f2d19d3b455e953297ad
Benedict Robinson <benedict.r1713@gmail.com>
2025-01-02 10:14:25 +0000

    User page styled

--- src/Compenents/UserPage.jsx
+++ src/Compenents/UserPage.jsx
@@ -53,11 +53,11 @@ export default function UserPage() {
           </div>
           <div className="stats">
             <h3>Stats</h3>
-            <p>Articles: &nbsp; {userArticles.length}</p>
-            <p>Votes on Articles: &nbsp; {articleVotes}</p>
-            <p>Comments: &nbsp; {userComments.length}</p>
-            <p>Votes on Comments: &nbsp; {commentVotes}</p>
-            <p>Total Votes: &nbsp; {commentVotes + articleVotes}</p>
+            <p id="stat-name">Articles &nbsp; <span id="stat-value">{userArticles.length}</span></p>
+            <p id="stat-name">Votes on Articles &nbsp; <span id="stat-value">{articleVotes}</span></p>
+            <p id="stat-name">Comments &nbsp; <span id="stat-value">{userComments.length}</span></p>
+            <p id="stat-name">Votes on Comments &nbsp; <span id="stat-value">{commentVotes}</span></p>
+            <p id="stat-name">Total Votes &nbsp; <span id="stat-value">{commentVotes + articleVotes}</span></p>
           </div>
         </div>
         <div className="profile-right">
