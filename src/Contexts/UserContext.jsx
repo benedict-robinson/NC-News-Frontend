5755bc55b41a36d383e35c0637ad4319da6cb6a6
Benedict Robinson <benedict.r1713@gmail.com>
2025-01-08 16:43:46 +0000

    Bug Fix

--- src/Contexts/UserContext.jsx
+++ src/Contexts/UserContext.jsx
@@ -4,17 +4,15 @@ import { getUsers } from "../api";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const name = localStorage.getItem("name")
-  const [username, setUsername] = useState(() => {
-    return name || "tickle122"
-  })
+  const name = localStorage.getItem("name") || "tickle122"
+  
   const [user, setNewUser] = useState({});
   useEffect(() => {
     getUsers().then((response) => {
-      const [currentUser] = response.filter(e => e.username === username)
+      const [currentUser] = response.filter(e => e.username === name)
       setUser(currentUser)
     })
-  }, [])
+  }, [name])
 
   const setUser = (newUser) => {
     localStorage.setItem("name", newUser.username)
