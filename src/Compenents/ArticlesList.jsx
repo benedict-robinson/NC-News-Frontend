925a20ad3b5475e7a28762366ed75e2e1f5fc292
Benedict Robinson <benedict.r1713@gmail.com>
2025-01-10 10:47:26 +0000

    Search Bar Bug Fixed

--- src/Compenents/ArticlesList.jsx
+++ src/Compenents/ArticlesList.jsx
@@ -3,7 +3,14 @@ import ArticleCard from "./ArticleCard"
 
 
 export default function ArticlesList({articles, searchTerm}) {
-
+  if (articles.length === 0) {
+    return (
+      <div>
+      <h2>{searchTerm ? `Showing results for '${searchTerm}'` : ""}</h2>
+        <p>No Articles Found</p>
+    </div>
+    )
+  }
   return (
     <div>
       <h2>{searchTerm ? `Showing results for '${searchTerm}'` : ""}</h2>
--- src/Compenents/SearchBar.jsx
+++ src/Compenents/SearchBar.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react"
-import { useSearchParams } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 
 export default function SearchBar() {
     const [searchParams, setSearchParams] = useSearchParams()
     const [searchTerm, setSearchTerm] = useState("")
+    const navigate = useNavigate()
 
     function handleInput(e) {
         setSearchTerm(e.target.value)
     }
     function handleSubmit() {
-        setSearchParams(`?search=${searchTerm}`)
+        // setSearchParams(`?search=${searchTerm}`)
+        navigate(`/articles?search=${searchTerm}`)
     }
     function handleEnterKey(e) {
         if (e.key === "Enter") {
